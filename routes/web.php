440663db34f4255df7d089a9213d4c2596b2c30c
keiko.otani <queserasera425@gmail.com>
2022-03-04 01:38:00 +0900

    ルーティング記述

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', HelloController::class);
+Route::get('/goodmorning', GoodMorningController::class);
+Route::get('/goodnight', GoodNightController::class);
+Route::get('/thankyou', ThankYouController::class);
