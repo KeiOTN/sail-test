91297e84983fa58b94a9b99ac607484c8a574dc8
keiko.otani <queserasera425@gmail.com>
2022-03-04 01:04:34 +0900

    API動作確認練習用

---
+++ app/Http/Controllers/Api/MemberController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Models\Member;
+use App\Http\Controllers\Controller;
+
+class MemberController extends Controller
+{
+     /**
+     * メンバー作成.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function create(Request $request) {
+        $member = Member::create([
+            'name' => $request->name,
+            'gender' => $request->gender,
+            'age' => $request->age,
+            'address' => $request->address,
+            'tel' => $request->tel,
+        ]);
+
+        return response()->json($member);
+    }
+
+    /**
+     * メンバー取得.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function fetch(Request $request) {
+        $member = Member::find($request->member_id);
+        return response()->json($member);
+    }
+
+    /**
+     * メンバー更新.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function update(Request $request) {
+        $member = Member::find($request->member_id);
+        $member->update([
+            'name' => $request->name,
+            'gender' => $request->gender,
+            'age' => $request->age,
+            'address' => $request->address,
+            'tel' => $request->tel,
+        ]);
+
+        return response()->json($member);
+    }
+
+    /**
+     * メンバー削除.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function delete(Request $request) {
+        $member = Member::find($request->member_id);
+        $member->delete();
+        return response()->json();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\MemberController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// memberのCRUD
+Route::post('member', [MemberController::class, 'create']);
+Route::get('member', [MemberController::class, 'fetch']);
+Route::put('member', [MemberController::class, 'update']);
+Route::delete('member', [MemberController::class, 'delete']);
